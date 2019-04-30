8d7e9b62ac8ca0793080e78aa4315eecb2668836
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-30 23:25:05 +0000

    fixed circular import, created handler for 404 errors, created a new view for State objects that handles GET, PUT, DELETE

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 app factory
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 import os
 from models import storage
 
@@ -13,9 +13,15 @@ def create_app():
 
     # Creates a Flask instance
     app = Flask(__name__)
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app.url_map.strict_slashes = False
 
     # Registers blueprint to our Flask instance
     app.register_blueprint(app_views)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return jsonify(error="Not found"), 404
 
     @app.teardown_appcontext
     def teardown_db(error):
@@ -31,5 +37,5 @@ if __name__ == "__main__":
         os.environ['HBNB_API_HOST'] = '0.0.0.0'
     if 'HBNB_API_PORT' not in os.environ:
         os.environ['HBNB_API_PORT'] = 5000
-    app.run(host=os.environ['HBNB_API_HOST'], port=os.environ['HBNB_API_PORT'],
-            threaded=True)
+    app.run(debug=True, host=os.environ['HBNB_API_HOST'],
+            port=os.environ['HBNB_API_PORT'], threaded=True)
