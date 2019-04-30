1b11954eff7d39d7c91b206df2e5383a7151b6a2
Wendy Leung <wendy.leung@holbertonschool.com>
2019-04-30 23:57:21 +0000

    modify states to handle POST

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,15 +13,23 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route('/states')
+@app_views.route('/states', methods=['GET', 'POST'])
 def states():
     """Retrieves list of all State objects"""
+    if request.method == 'POST':
+        x = request.get_json()
+        if request.is_json is False:
+            return jsonify(error="Not a JSON"), 400
+        if 'name' not in x.keys():
+            return jsonify(error="Missing name"), 400
+        new_state = State(**x)
+        return jsonify(new_state.to_dict()), 201
     states_dict = storage.all()
     return jsonify([item.to_dict() for item in states_dict.values()])
 
 
 @app_views.route('/states/<state_id>',
-                 methods=['GET', 'POST', 'PUT', 'DELETE'])
+                 methods=['GET', 'PUT', 'DELETE'])
 def state_id(state_id):
     """Retrieves a State object"""
     if request.method != 'POST':
@@ -43,11 +51,3 @@ def state_id(state_id):
                 if key not in ignore:
                     setattr(state, key, value)
             return jsonify(state.to_dict()), 200
-    if request.method == 'POST':
-        x = request.get_json()
-        if request.is_json is False:
-            return jsonify(error="Not a JSON"), 400
-        if 'name' not in x.keys():
-            return jsonify(error="Missing name"), 400
-        new_state = State(**x)
-        return jsonify(new_state.to_dict()), 201
